debb425a6bee21bd74b64a9df98254742acb2f12
Satyam2129 <satyam.singh_cs20@gla.ac.in>
2021-12-08 19:00:16 +0530

    Exception handled

--- src/JDBC_Test.java
+++ src/JDBC_Test.java
@@ -1,6 +1,12 @@
 import java.sql.*;
 import java.util.Scanner;
 
+class Custom_Exeption extends Exception{
+    Custom_Exeption(String s){
+        super(s);
+    }
+}
+
 public class JDBC_Test {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
@@ -12,16 +18,26 @@ public class JDBC_Test {
         int a=0;
         for(int i=0;i<3;i++)
         {
-            String s1=sc.nextLine();
-            if(s1.matches("[a-zA-Z0-9]+(@gmail.com|@rediff.com|@outlook.com)"))
-                str[a++]=s1;
+            try
+            {
+            String s1 = sc.nextLine();
+            if (s1.matches("[a-zA-Z0-9]+(@gmail.com|@rediff.com|@outlook.com)"))
+            {
+                str[a++] = s1;
+            } else
+            {
+                throw new Custom_Exeption("Invalid Input");
+            }
+            }
+            catch (Custom_Exeption obj)
+            {
+            System.out.println(obj);
+            }
         }
-
         for(int i=0;i<a;i++)
         {
             s.setString(1,str[i]);
             s.execute();
         }
-
     }
 }
